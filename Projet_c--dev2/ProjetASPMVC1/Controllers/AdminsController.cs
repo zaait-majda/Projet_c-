22a9827cf900a4b8f747f6cb5118fbd6a382a282
ManaleAit <58122529+ManaleAit@users.noreply.github.com>
2020-03-22 17:10:43 +0100

    changement projet

---
+++ Projet_c--dev2/ProjetASPMVC1/Controllers/AdminsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ProjetASPMVC1.Models;
+
+namespace ProjetASPMVC1.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private Projet_ContextBD db = new Projet_ContextBD();
+
+        public ActionResult LoginAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorise(Admin admin)
+        {
+            //if (ModelState.IsValid)
+            //    {
+            using (Projet_ContextBD db = new Projet_ContextBD())
+            {
+
+                var userDetail = db.Admins.Where(x => x.Login == admin.Login && x.password == admin.password).FirstOrDefault();
+                if (userDetail == null)
+                {
+
+                    // user.LoginErrorMsg = "Invalid UserName or Password";
+                    Response.Write("<script>alert(\'données incorrect\');</" + "script>");
+                    return View("Index", admin);
+                }
+                else
+                {
+
+                    Session["login"] = userDetail.Login;
+                    Session["password"] = userDetail.password;
+                    return RedirectToAction("Next_page", new RouteValueDictionary(new {Action = "Index", id = Session["login"] }));
+
+                }
+
+            }
+
+
+
+
+        }
+
+
+
+            // GET: Admins
+            public ActionResult Index(string id)
+        {
+            ViewBag.login = id;
+            return View(db.Admins.ToList());
+        }
+
+        // GET: Admins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // GET: Admins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admins/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Login,password")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Admins.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(admin);
+        }
+
+        // GET: Admins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admins/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Login,password")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        // GET: Admins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Admin admin = db.Admins.Find(id);
+            db.Admins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Projet_c--dev2/ProjetASPMVC1/Controllers/CandidatsController.cs
@@ -0,0 +1,324 @@
+﻿using ProjetASPMVC1.Models;
+using Rotativa;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Hosting;
+
+namespace ProjetASPMVC1.Controllers
+{
+    public class CandidatsController : Controller
+    {
+        public static String id;
+        private Projet_ContextBD db = new Projet_ContextBD();
+        public ActionResult Index(Candidat objUser)
+        {
+
+            return View();
+
+
+        }
+        [HttpPost]
+        public ActionResult Authorise(Candidat user)
+        {
+            using (Projet_ContextBD db = new Projet_ContextBD())
+            {
+                var userDetail = db.Candidats.Where(x => x.CIN == user.CIN && x.password == user.password).FirstOrDefault();
+                if (userDetail == null)
+                {
+                    // user.LoginErrorMsg = "Invalid UserName or Password";
+                    return View("Index", user);
+                }
+                else
+                {
+                    Session["CIN"] = user.CIN;
+                    Session["prenom"] = user.prenom;
+                    return RedirectToAction("Edit", "Candidats");
+                }
+            }
+
+        }
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["CIN"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Creation()
+        {
+            
+            ViewBag.f = new SelectList(db.Filieres, "id_fil", "nom_fil");
+            return View();
+        }
+        public ActionResult Creation2()
+        {
+
+            ViewBag.f = new SelectList(db.Filieres, "id_fil", "nom_fil");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Creation(
+
+           string CIN, string CNE, string  prenom, 
+           string  nom, string  ville, string  addresse, string  tel,
+           string GSM, string  type_bac,
+           string annee_bac, string note_bac, string mention_bac, 
+           string  nationnalite, string  sexe, string  password, string  password_conf, string  photo,
+           string  email,  string  niveau, string  date_naiss,
+           string  id_fil, string s1, string s2, string s3, string s4, string s5, string s6,string nom_dip,
+           string villeDip, string  etab
+
+
+            )
+        {
+
+            Candidat cand = new Candidat();
+            Notes note = new Notes();
+            Diplome dip = new Diplome();
+            note.s1 = Convert.ToDouble(s1);
+            note.s2 = Convert.ToDouble(s2);
+            note.s3 = Convert.ToDouble(s3);
+            note.s4 = Convert.ToDouble(s4);
+            note.s5 = Convert.ToDouble(s5);
+            note.s6 = Convert.ToDouble(s6);
+            dip.nom_diplome = nom_dip;
+            dip.ville_diplome = ville;
+            dip.etablissement = etab;
+            
+            cand.id_fil =Convert.ToInt32(id_fil);
+           
+            cand.CNE = CNE;
+            cand.CIN = CIN;
+            cand.prenom = prenom;
+            cand.nom = nom;
+            cand.ville = ville;
+            cand.addresse = addresse;
+            cand.tel = tel;
+            cand.GSM = GSM;
+            cand.type_bac = type_bac;
+            cand.annee_bac = annee_bac;
+            cand.note_bac = note_bac;
+            cand.mention_bac = mention_bac;
+            cand.n_dossier = "";
+           
+            cand.nationnalite = nationnalite;
+            cand.sexe = sexe;
+            cand.cont_sup = 0;
+
+            cand.cont_ajout= 0;
+            cand.password = password;
+            cand.password_conf = password_conf;
+            cand.photo = photo;
+            cand.email = email;
+            cand.statut = "inscrit";
+            cand.EmailConfirmed = true;
+
+            cand.niveau = niveau;
+            cand.date_naiss =Convert.ToDateTime(date_naiss);
+            db.Notes.Add(note);
+            db.SaveChanges();
+            db.Diplomes.Add(dip);
+            db.SaveChanges();
+            cand.id_diplome = dip.id_diplome;
+            cand.id_note = note.id_note;
+            db.Candidats.Add(cand);
+            db.SaveChanges();
+            sendmail(cand);
+            return RedirectToAction("Index","Home");
+        }
+
+
+        [HttpPost]
+        public ActionResult Creation2(
+
+            string CIN, string CNE, string prenom,
+            string nom, string ville, string addresse, string tel,
+            string GSM, string type_bac,
+            string annee_bac, string note_bac, string mention_bac,
+            string nationnalite, string sexe, string password, string password_conf, string photo,
+            string email, string niveau, string date_naiss,
+            string id_fil, string s1, string s2, string s3, string s4, string s5, string s6, string nom_dip,
+            string villeDip, string etab
+
+
+             )
+        {
+
+            Candidat cand = new Candidat();
+            Notes note = new Notes();
+            Diplome dip = new Diplome();
+            note.s1 = Convert.ToDouble(s1);
+            note.s2 = Convert.ToDouble(s2);
+            note.s3 = Convert.ToDouble(s3);
+            note.s4 = Convert.ToDouble(s4);
+            note.s5 = Convert.ToDouble(s5);
+            note.s6 = Convert.ToDouble(s6);
+            dip.nom_diplome = nom_dip;
+            dip.ville_diplome = ville;
+            dip.etablissement = etab;
+
+            cand.id_fil = Convert.ToInt32(id_fil);
+
+            cand.CNE = CNE;
+            cand.CIN = CIN;
+            cand.prenom = prenom;
+            cand.nom = nom;
+            cand.ville = ville;
+            cand.addresse = addresse;
+            cand.tel = tel;
+            cand.GSM = GSM;
+            cand.type_bac = type_bac;
+            cand.annee_bac = annee_bac;
+            cand.note_bac = note_bac;
+            cand.mention_bac = mention_bac;
+            cand.n_dossier = "";
+
+            cand.nationnalite = nationnalite;
+            cand.sexe = sexe;
+            cand.cont_sup = 0;
+
+            cand.cont_ajout = 0;
+            cand.password = password;
+            cand.password_conf = password_conf;
+            cand.photo = photo;
+            cand.email = email;
+            cand.statut = "Candidat";
+            cand.niveau = niveau;
+            cand.date_naiss = Convert.ToDateTime(date_naiss);
+            db.Notes.Add(note);
+            db.SaveChanges();
+            db.Diplomes.Add(dip);
+            db.SaveChanges();
+            cand.id_diplome = dip.id_diplome;
+            cand.id_note = note.id_note;
+            db.Candidats.Add(cand);
+            db.SaveChanges();
+            sendmail(cand);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public ActionResult Next_page(String id)
+
+        {
+
+
+            Candidat candidat = db.Candidats.Find(id);
+            if (candidat == null)
+            {
+                return HttpNotFound();
+            }
+        
+            return View(candidat);
+        }
+        public ActionResult EXportPDF()
+        {
+            id = (string)Session["CIN"];
+
+            return new ActionAsPdf("fiche2")
+            {
+                FileName = Server.MapPath("~/Content/fiche.pdf")
+            };
+            //frm
+        }
+
+        public ActionResult fiche2()
+
+        {
+            String s = CandidatsController.id;
+            Candidat candidat = db.Candidats.Find(s);
+
+            return View(candidat);
+        }
+        public void sendmail(Candidat ca)
+        {
+            string body = System.IO.File.ReadAllText(HostingEnvironment.MapPath("~/EmailTemplate/") + "Text" + ".cshtml");
+
+            var fromAddress = new MailAddress("testensa2020@gmail.com", "ENSAS");
+            var toAddress = new MailAddress(ca.email, "To Name");
+            const string fromPassword = "majda2018";
+            const string subject = "Subject";
+            var url = "http://localhost:56154/" + "Candidats/Confirm?regId=" + ca.CIN;
+            body = body.Replace("@ViewBag.ConfirmationLink", url);
+            var nom = "PASS  HI " + ca.nom;
+            body += body.Replace("@ViewBag.message1", nom);
+            var PASS = "votre mot de passe " + ca.password;
+            body += body.Replace("@ViewBag.message", PASS);
+            body = body.ToString();
+            //  BuildEmailTemplate(ca.CIN);
+
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+
+            }
+        }
+
+        public ActionResult Confirm(String regId)
+        {
+            ViewBag.regID = regId;
+            return View();
+        }
+        public JsonResult RegisterConfirm(String regId)
+        {
+
+            using (var ddb = new Projet_ContextBD())
+            {
+                Candidat Data = ddb.Candidats.Where(x => x.CIN == regId).FirstOrDefault();
+                Data.EmailConfirmed = true;
+
+                db.SaveChanges();
+                var msg = "Your Email Is Verified!";
+                return Json(msg, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public ActionResult Change(String LangugeAbbrevation)
+        {
+            if (LangugeAbbrevation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LangugeAbbrevation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LangugeAbbrevation);
+
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = LangugeAbbrevation;
+            Response.Cookies.Add(cookie);
+
+
+
+            return View("Index");
+
+        }
+
+        public ActionResult Edit()//your view page
+        {
+            return View();
+        }
+    }
+}
